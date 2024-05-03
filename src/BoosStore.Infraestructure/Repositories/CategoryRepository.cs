898ee41a75d359d36dddc6d7ec91cff8518e9dbd
Enrique Mainero Salinas <emainero.salinas@gmail.com>
2024-05-03 06:24:49 -0600

    Created Interfaces, Services and Repositories

---
+++ src/BoosStore.Infraestructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Interfaces;
+using BookStore.Domain;
+using BookStore.Infraestructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infraestructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext context) : base(context) { }
+    }
+}
